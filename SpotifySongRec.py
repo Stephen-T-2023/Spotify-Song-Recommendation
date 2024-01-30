b00232ef48a6d21fdf3b5558a419fd5f3c0133e5
Stephen-T-2023 <144911216+Stephen-T-2023@users.noreply.github.com>
2024-01-30 13:52:02 +0000

    Add files via upload

--- SpotifySongRec.py
+++ SpotifySongRec.py
@@ -2,12 +2,21 @@ import pandas as pd
 from sklearn.metrics.pairwise import sigmoid_kernel
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn import preprocessing
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy import Spotify
+
+sp_oauth = SpotifyOAuth(client_id = 'd46e3db18c384e6ab33d369018d45fe0', client_secret = 'e8b047acdf984f0785e6f70ef58bb4df', redirect_uri = "https://stephen-t-2023.github.io/Spotify-Song-Recommendation/")
+
+access_token = sp_oauth.get_cached_token()
+
+sp = Spotify(auth_manager = sp_oauth)
+
+playlists = sp.user_playlists("Mr_Cheesicus")
 
 df = pd.read_csv("data.csv")
 
 df.head()
-
-df.info()
 
 feature_cols = ['acousticness', 'danceability', 'duration_ms', 'energy',
                 'instrumentalness', 'key', 'liveness', 'loudness', 'mode',
@@ -16,8 +25,6 @@ feature_cols = ['acousticness', 'danceability', 'duration_ms', 'energy',
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 normalised_df = scaler.fit_transform(df[feature_cols])
-
-print(normalised_df[:2])
 
 indices = pd.Series(df.index, index = df['song_title']).drop_duplicates()
 
